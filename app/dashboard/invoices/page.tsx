4ee623c360cf654ae09c8b00183826cd38e2548c
samuelkamotho92 <samuelkamotho92@gmail.com>
2024-03-06 16:35:09 +0300

    Started working on dashboard

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>
+            <p>Invoices</p>
+        </div>
+    )
+}
+
+export default Page
---
+++ app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <p>Register Page</p>
+        </div>
+    )
+}
+
+export default page
